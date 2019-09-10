35dd79e9ae09dc2124fb48adabee0ff13fb234c9
Claire Dong <claire.dong@duke.edu>
2019-09-10 10:19:23 -0400

    Added BMI_Calculator function to calculate BMI value and analysis based on given weight and height

--- BMICalculator.py
+++ BMICalculator.py
@@ -1,18 +1,33 @@
+def BMI_Calculator(weight,height):
+    value = weight/(height*height)
+    if value < 18.5:
+        result = "underweight"
+    elif 18.5 <= value < 25:
+        result = "normal weight"
+    elif 25 <= value < 30:
+        result = "overweight"
+    else:
+        result = "obese"
+    return value, result
+
 def BMI_DataEntry():
     w_input = input("\nEnter your weight in kg/lb (eg. 130 lb): ")
     w_data = w_input.split(" ")
     if w_data[1] == "lb":
-        weight = float(w_data[0])/2.20462
+        weight = float(w_data[0])/2.20462 #convert lb to kg
     else:
         weight = float(w_data[0])
     h_input = input("Enter your height in m/in (eg. 65 in): ")
     h_data = h_input.split(" ")
     if h_data[1] == "in":
-        height = float(h_data[0])/39.37
+        height = float(h_data[0])/39.37 #convert in to m
     else:
         height = float(h_data[0])
-    print("\nThe entered weight is {} kg".format(weight))
-    print("The entered height is {} m\n".format(height))
+    value, result = BMI_Calculator(weight,height)
+    print("\nThe entered weight is {} {}".format(w_data[0], w_data[1]))
+    print("The entered height is {} {}".format(h_data[0], h_data[1]))
+    print("The calculated BMI is {}".format(value))
+    print("The result is {}\n".format(result))
 
 def interface():
         print("Welcome to the BMI Calculator!")
